f242f870e0fbffe2fa19d52f358ccd20c23edeff
sugi <santanu2481995@gmail.com>
2019-11-05 12:17:47 +0530

    Create file strucutre

---
+++ src/algoD.nim
@@ -0,0 +1,8 @@
+# This is just an example to get you started. A typical library package
+# exports the main API in this file. Note that you cannot rename this file
+# but you can remove it if you wish.
+
+proc addp*(x, y: int): int =
+  ## Adds two files together.
+  echo "hello wrold"
+  return x + y
