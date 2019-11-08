bf4560f3e755423b0e9bafec2e2ca5dde997d943
sugi <santanu2481995@gmail.com>
2019-11-08 23:16:19 +0530

    Corrected dfs

--- src/algoD/bst/depthFirstTree.nim
+++ src/algoD/bst/depthFirstTree.nim
@@ -4,16 +4,16 @@ import node
 proc inOrderTraversal*[T](this: BST[T]): seq[T]= 
     proc helperRecc[T](node: NodeBst[T], resultSeq: var seq[T]) =
         if not isNil(node):
-            resultSeq.add(node.val)
             helperRecc(node.l_child, resultSeq)
+            resultSeq.add(node.val)
             helperRecc(node.r_child, resultSeq)
     this.root.helperRecc(result)
 
 proc preOrderTraversal*[T](this: BST[T]):seq[T] =
     proc helperRecc[T](node: NodeBst[T], resultSeq: var seq[T]) =
         if not isNil(node):
-            helperRecc(node.l_child, resultSeq)
             resultSeq.add(node.val)
+            helperRecc(node.l_child, resultSeq)
             helperRecc(node.r_child, resultSeq)
     this.root.helperRecc(result)
 
@@ -24,5 +24,3 @@ proc postOrderTraversal*[T](this: BST[T]):seq[T] =
             helperRecc(node.r_child, resultSeq)
             resultSeq.add(node.val)
     this.root.helperRecc(result)
-    
-
